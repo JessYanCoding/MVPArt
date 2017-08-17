154d2e0d3b161bfc4d91a97e30b508b41473fe68
jessyan <jessyan@foxmail.com>
2017-08-17 14:35:47 +0800

    Improve model template

--- app/src/main/java/me/jessyan/mvpart/demo/mvp/model/UserRepository.java
+++ app/src/main/java/me/jessyan/mvpart/demo/mvp/model/UserRepository.java
@@ -15,12 +15,11 @@ import me.jessyan.mvpart.demo.mvp.model.api.service.UserService;
 import me.jessyan.mvpart.demo.mvp.model.entity.User;
 
 /**
- *
  * 必须实现 IModel
  * 可以根据不同的业务逻辑划分多个 Repository 类,多个业务逻辑相近的页面可以使用同一个 Repository 类
  * 无需每个页面都创建一个独立的 Repository
  * 通过 {@link IRepositoryManager#createRepository(java.lang.Class)} 获得的 Repository 实例,为单例对象
- *
+ * <p>
  * Created by jess on 9/4/16 10:56
  * Contact with jess.yan.effort@gmail.com
  */
@@ -31,6 +30,7 @@ public class UserRepository implements IModel {
 
     /**
      * 必须含有一个接收IRepositoryManager接口的构造函数,否则会报错
+     *
      * @param manager
      */
     public UserRepository(IRepositoryManager manager) {
@@ -57,6 +57,6 @@ public class UserRepository implements IModel {
 
     @Override
     public void onDestroy() {
-
+        this.mManager = null;
     }
 }
