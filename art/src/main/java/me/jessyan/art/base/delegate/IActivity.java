9650ddb3f4c95ac5156e2551ea1e6d981c4bcf25
JessYan <jessyan@foxmail.com>
2018-04-11 12:03:30 +0800

    Improve IActivity

--- art/src/main/java/me/jessyan/art/base/delegate/IActivity.java
+++ art/src/main/java/me/jessyan/art/base/delegate/IActivity.java
@@ -60,7 +60,7 @@ public interface IActivity<P extends IPresenter> {
     boolean useEventBus();
 
     /**
-     * 初始化 View,如果initView返回0,框架则不会调用{@link android.app.Activity#setContentView(int)}
+     * 初始化 View, 如果 {@link #initView(Bundle)} 返回 0, 框架则不会调用 {@link Activity#setContentView(int)}
      *
      * @param savedInstanceState
      * @return
