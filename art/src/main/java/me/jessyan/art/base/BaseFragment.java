23c2cafb21239286ad866bfcbd74880a7f655378
JessYan <jessyan@foxmail.com>
2018-04-11 10:45:52 +0800

    Improve BaseFragment

--- art/src/main/java/me/jessyan/art/base/BaseFragment.java
+++ art/src/main/java/me/jessyan/art/base/BaseFragment.java
@@ -55,12 +55,12 @@ public abstract class BaseFragment<P extends IPresenter> extends Fragment implem
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return initView(inflater, container, savedInstanceState);
     }
 
     @Override
-    public void setPresenter(P presenter) {
+    public void setPresenter(@Nullable P presenter) {
         this.mPresenter = presenter;
     }
 
