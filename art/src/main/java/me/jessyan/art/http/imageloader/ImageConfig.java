4a164f8819b7d14ed77d1fa8cf808dda018d5410
JessYan <jessyan@foxmail.com>
2018-04-11 15:32:16 +0800

    Improve ImageConfigImpl and ImageConfig

--- art/src/main/java/me/jessyan/art/http/imageloader/ImageConfig.java
+++ art/src/main/java/me/jessyan/art/http/imageloader/ImageConfig.java
@@ -32,9 +32,6 @@ public class ImageConfig {
     protected ImageView imageView;
     protected int placeholder;//占位符
     protected int errorPic;//错误占位符
-    protected boolean isClearMemory;//清理内存缓存
-    protected boolean isClearDiskCache;//清理本地缓存
-
 
     public String getUrl() {
         return url;
@@ -50,13 +47,5 @@ public class ImageConfig {
 
     public int getErrorPic() {
         return errorPic;
-    }
-
-    public boolean isClearMemory() {
-        return isClearMemory;
-    }
-
-    public boolean isClearDiskCache() {
-        return isClearDiskCache;
     }
 }
