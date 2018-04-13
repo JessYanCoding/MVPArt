53a0b6c9a587f9317a2c6feb9c1fb7cdc38ef2ff
JessYan <jessyan@foxmail.com>
2018-04-13 17:11:01 +0800

    Inject IntelligentCache

--- art/src/main/java/me/jessyan/art/di/module/GlobalConfigModule.java
+++ art/src/main/java/me/jessyan/art/di/module/GlobalConfigModule.java
@@ -39,6 +39,7 @@ import me.jessyan.art.http.log.FormatPrinter;
 import me.jessyan.art.http.log.RequestInterceptor;
 import me.jessyan.art.integration.cache.Cache;
 import me.jessyan.art.integration.cache.CacheType;
+import me.jessyan.art.integration.cache.IntelligentCache;
 import me.jessyan.art.integration.cache.LruCache;
 import me.jessyan.art.utils.DataHelper;
 import me.jessyan.art.utils.Preconditions;
@@ -214,9 +215,18 @@ public class GlobalConfigModule {
             @NonNull
             @Override
             public Cache build(CacheType type) {
-                //若想自定义 LruCache 的 size,或者不想使用 LruCache, 想使用自己自定义的策略
-                //请使用 GlobalConfigModule.Builder#cacheFactory() 扩展
-                return new LruCache(type.calculateCacheSize(application));
+                //若想自定义 LruCache 的 size, 或者不想使用 LruCache, 想使用自己自定义的策略
+                //使用 GlobalConfigModule.Builder#cacheFactory() 即可扩展
+                switch (type.getCacheTypeId()){
+                    //Activity、Fragment 以及 Extras 使用 IntelligentCache (具有 LruCache 和 可永久存储数据的 Map)
+                    case CacheType.EXTRAS_TYPE_ID:
+                    case CacheType.ACTIVITY_CACHE_TYPE_ID:
+                    case CacheType.FRAGMENT_CACHE_TYPE_ID:
+                        return new IntelligentCache(type.calculateCacheSize(application));
+                    //其余使用 LruCache (当达到最大容量时可根据 LRU 算法抛弃不合规数据)
+                    default:
+                        return new LruCache(type.calculateCacheSize(application));
+                }
             }
         } : mCacheFactory;
     }
@@ -322,9 +332,5 @@ public class GlobalConfigModule {
         public GlobalConfigModule build() {
             return new GlobalConfigModule(this);
         }
-
-
     }
-
-
 }
