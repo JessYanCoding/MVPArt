bdc0d885cedcb26f099c3bb23c89093a4147787e
JessYan <jessyan@foxmail.com>
2018-04-13 18:19:53 +0800

    Used IntelligentCache on FragmentLifecycle

--- art/src/main/java/me/jessyan/art/integration/FragmentLifecycle.java
+++ art/src/main/java/me/jessyan/art/integration/FragmentLifecycle.java
@@ -29,6 +29,7 @@ import me.jessyan.art.base.delegate.FragmentDelegate;
 import me.jessyan.art.base.delegate.FragmentDelegateImpl;
 import me.jessyan.art.base.delegate.IFragment;
 import me.jessyan.art.integration.cache.Cache;
+import me.jessyan.art.integration.cache.IntelligentCache;
 import me.jessyan.art.utils.Preconditions;
 import timber.log.Timber;
 
@@ -57,7 +58,9 @@ public class FragmentLifecycle extends FragmentManager.FragmentLifecycleCallback
             if (fragmentDelegate == null || !fragmentDelegate.isAdded()) {
                 Cache<String, Object> cache = getCacheFromFragment((IFragment) f);
                 fragmentDelegate = new FragmentDelegateImpl(fm, f);
-                cache.put(FragmentDelegate.FRAGMENT_DELEGATE, fragmentDelegate);
+                //使用 IntelligentCache.KEY_KEEP 作为 key 的前缀, 可以使储存的数据永久存储在内存中
+                //否则存储在 LRU 算法的存储空间中, 前提是 Fragment 使用的是 IntelligentCache (框架默认使用)
+                cache.put(IntelligentCache.KEY_KEEP + FragmentDelegate.FRAGMENT_DELEGATE, fragmentDelegate);
             }
             fragmentDelegate.onAttach(context);
         }
@@ -165,7 +168,7 @@ public class FragmentLifecycle extends FragmentManager.FragmentLifecycleCallback
     private FragmentDelegate fetchFragmentDelegate(Fragment fragment) {
         if (fragment instanceof IFragment) {
             Cache<String, Object> cache = getCacheFromFragment((IFragment) fragment);
-            return (FragmentDelegate) cache.get(FragmentDelegate.FRAGMENT_DELEGATE);
+            return (FragmentDelegate) cache.get(IntelligentCache.KEY_KEEP + FragmentDelegate.FRAGMENT_DELEGATE);
         }
         return null;
     }
