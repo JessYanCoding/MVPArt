5a28a2bf3b04ee04e62e6300549f37fb531e44ce
JessYan <jessyan@foxmail.com>
2018-07-26 16:43:56 +0800

    Improve GlobalConfiguration's comments

--- demo/src/main/java/me/jessyan/mvpart/demo/app/GlobalConfiguration.java
+++ demo/src/main/java/me/jessyan/mvpart/demo/app/GlobalConfiguration.java
@@ -47,6 +47,8 @@ import me.jessyan.retrofiturlmanager.RetrofitUrlManager;
  *
  * @see AppDelegate
  * @see ManifestParser
+ * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki">请配合官方 Wiki 文档学习本框架 (Arms 的文档除了 MVP 部分, 其他的文档内容 Art 和 Arms 都可以共用)</a>
+ * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki/Issues">常见 Issues, 踩坑必看!</a>
  * Created by JessYan on 12/04/2017 17:25
  * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
  * <a href="https://github.com/JessYanCoding">Follow me</a>
