f5975d3989ae7c64999aa361dcf120af16fbd23c
JessYan <jessyan@foxmail.com>
2018-12-11 14:44:38 +0800

    Add @NonNull on GlideAppliesOptions

--- art/src/main/java/me/jessyan/art/http/imageloader/glide/GlideAppliesOptions.java
+++ art/src/main/java/me/jessyan/art/http/imageloader/glide/GlideAppliesOptions.java
@@ -16,6 +16,7 @@
 package me.jessyan.art.http.imageloader.glide;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
@@ -32,7 +33,6 @@ import me.jessyan.art.http.imageloader.BaseImageLoaderStrategy;
  * <a href="https://github.com/JessYanCoding">Follow me</a>
  * ================================================
  */
-
 public interface GlideAppliesOptions {
 
     /**
@@ -41,5 +41,5 @@ public interface GlideAppliesOptions {
      * @param context
      * @param builder {@link GlideBuilder} 此类被用来创建 Glide
      */
-    void applyGlideOptions(Context context, GlideBuilder builder);
+    void applyGlideOptions(@NonNull Context context, @NonNull GlideBuilder builder);
 }
